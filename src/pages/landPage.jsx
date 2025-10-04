e478f8a2aadbcea8d6a53734dfcaf7335403aa79
zalhashfi <rizalhashfi@gmail.com>
2025-10-04 19:56:02 +0700

    landpage 2.0

--- src/pages/landPage.jsx
+++ src/pages/landPage.jsx
@@ -1,5 +1,7 @@
 import '../index.css'
 import Navbar from '../components/navbar'
+import BaseCard from '../components/BaseCard'
+import '../../public/gambarKos.png'
 
 export default function landPage() {
 
@@ -11,7 +13,38 @@ export default function landPage() {
                 <p className='font-sans text-3xl font-bold'>Your Next Room is Just a Click Away</p>
             </div>
         </div>
-        <div className='h-50 bg-cream'></div>
+        <div className='h-150 bg-cream flex justify-center items-center gap-75 '>
+            <div>
+                <BaseCard 
+                    image='gambarKos.png'
+                    title="Kos 1"
+                    description="Deskripsi singkat"
+                    price={1500000}
+                    location={"Jakarta, Indonesia"}
+                    additionalInfo={"Kamar mandi dalam, AC, WiFi"}
+                />
+            </div>
+            <div>
+                <BaseCard 
+                    image="gambarKos.png"
+                    title="Kos 2"
+                    description="Deskripsi singkat"
+                    price={1200000}
+                    location={"Bandung, Indonesia"}
+                    additionalInfo={"Kamar mandi luar, Non-AC, WiFi"}
+                />
+            </div>
+            <div>
+                <BaseCard
+                    image="gambarKos.png"
+                    title="Kos 3"
+                    description="Deskripsi singkat"
+                    price={1000000}
+                    location={"Surabaya, Indonesia"}
+                    additionalInfo={"Kamar mandi dalam, AC, No WiFi"}
+                />
+            </div>
+        </div>
         </>
     )
 }
